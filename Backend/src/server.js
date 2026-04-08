ecf5fe80bb7187f9a08e933f1bdc2508fb5ed106
Mohd Shahbaaz <shahbaazmohd08@gmail.com>
2026-04-08 15:41:38 +0530

    ready for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,8 +1,76 @@
-//const express = require('express');
+// //const express = require('express');
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+// // import path from 'path';
+// const express = require('express');
+
+// const express = require('express');
+// const authRoutes = require('./routes/auth.route.js');
+// const messageRoutes = require('./routes/message.route.js');
+
+// const app = express();
+// const __dirname = path.resolve();
+
+
+// const PORT = process.env.PORT || 3000;
+
+// app.use('/api/auth', authRoutes);
+// app.use('/api/messages', messageRoutes);
+
+// //make ready for deployment
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+//     });
+// }
+
+// app.listen(PORT, () => {
+//     console.log('Server is running on port ' + PORT);
+// });
+
+
+
+
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+// const express = require('express');
+// const path = require('path');
+
+// const authRoutes = require('./routes/auth.route.js');
+// const messageRoutes = require('./routes/message.route.js');
+
+// const app = express();
+
+// //const __dirname = path.resolve();
+// const PORT = process.env.PORT || 3000;
+
+// app.use('/api/auth', authRoutes);
+// app.use('/api/messages', messageRoutes);
+
+// // serve frontend
+// app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+// });
+
+// app.listen(PORT, () => {
+//     console.log('Server is running on port ' + PORT);
+// });
+
+
+
+
 const dotenv = require('dotenv');
 dotenv.config();
 
 const express = require('express');
+const path = require('path');
+
 const authRoutes = require('./routes/auth.route.js');
 const messageRoutes = require('./routes/message.route.js');
 
@@ -12,6 +80,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
+
+// serve frontend
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log('Server is running on port ' + PORT);
