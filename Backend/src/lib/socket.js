0e171d845516717a9e83c7c023c301b582045574
Mohd Shahbaaz <shahbaazmohd08@gmail.com>
2026-04-16 15:05:16 +0530

    online users implemented

--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -2,41 +2,44 @@ import { Server } from "socket.io";
 import http from "http";
 import express from "express";
 import { ENV } from "./env.js";
+import { socketAuthMiddleware } from "../middleware/socket.auth.middleware.js";
 
 const app = express();
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: {
-    origin: ENV.CLIENT_URL,
-    methods: ["GET", "POST"],
+    origin: [ENV.CLIENT_URL],
     credentials: true,
   },
 });
 
-const userSocketMap = {}; // {userId: socketId}
+// apply authentication middleware to all socket connections
+io.use(socketAuthMiddleware);
 
-export const getReceiverSocketId = (receiverId) => {
-  return userSocketMap[receiverId];
-};
+// we will use this function to check if the user is online or not
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId];
+}
+
+// this is for storig online users
+const userSocketMap = {}; // {userId:socketId}
 
 io.on("connection", (socket) => {
-  console.log("A user connected", socket.id);
+  console.log("A user connected", socket.user.fullName);
 
-  const userId = socket.handshake.query.userId;
-  if (userId) {
-    userSocketMap[userId] = socket.id;
-  }
+  const userId = socket.userId;
+  userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients
+  // io.emit() is used to send events to all connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
+  // with socket.on we listen for events from clients
   socket.on("disconnect", () => {
-    console.log("User disconnected", socket.id);
-    if (userId) {
-      delete userSocketMap[userId];
-      io.emit("getOnlineUsers", Object.keys(userSocketMap));
-    }
+    console.log("A user disconnected", socket.user.fullName);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
 
-export { app, server, io };
+export { io, app, server };
